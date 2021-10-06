3fd147898a70d490a969b81bc28603c7ad63effc
sovlookup <gonorth@qq.com>
2021-10-06 20:02:11 +0800

    :art: improve code

--- src/core/db/index.ts
+++ src/core/db/index.ts
@@ -21,31 +21,32 @@ abstract class RubickDB {
  * 为便于数据同步, 数据只会在内存中改动, 持久化需调用 updatePersistence 进行保存
  */
 export class CRDTDB extends RubickDB {
-	private doc: Y.Doc
+	private doc!: Y.Doc
 	private persistence: LeveldbPersistence
 	constructor(opt: { path: string; name: string }) {
 		super(opt)
 		this.persistence = new LeveldbPersistence(this.path, { level })
-		this.doc = new Y.Doc()
-		this.orGetPersistenceDB(this.name)
+		this.getOrNewPersistenceDB(this.name)
 	}
 
-	private async orGetPersistenceDB(name: string) {
-		const DBnames = await this.persistence.getAllDocNames()
-		if (DBnames.includes(name)) {
-			this.doc = await this.persistence.getYDoc(name)
-		}
+	private async getOrNewPersistenceDB(name: string) {
+		this.doc = await this.persistence.getYDoc(name)
 	}
 
-	// 将临时数据改动持久化
-	updatePersistence(doc?: Y.Doc) {
-		doc = doc || this.doc
-		return this.persistence.storeUpdate(this.name, Y.encodeStateAsUpdate(doc))
+	// 数据持久化
+	updatePersistence() {
+		return this.persistence.storeUpdate(this.name, Y.encodeStateAsUpdateV2(this.doc))
 	}
 
+	// 退出的时候数据持久化
+	exit() {
+		this.updatePersistence()
+	}
+
+	// todo 以 subdoc 为粒度跨设备同步，可选择开关
 	newMap(name: string) {
 		return new CRDTMap(this.doc, name)
 	}
 }
 
-// todo 不带同步功能的本地数据库
+// todo 本地数据库
