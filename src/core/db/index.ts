769a690ec762a51ed785ad4bdb633159db78dbfd
sovlookup <gonorth@qq.com>
2021-10-07 14:31:59 +0800

    :art: 优化代码结构 增加注释说明

--- src/core/db/index.ts
+++ src/core/db/index.ts
@@ -3,7 +3,7 @@ import { CRDTMap } from './map'
 import { LeveldbPersistence } from 'y-leveldb'
 import level from 'level-rocksdb'
 
-/** Rubick 数据库
+/** Rubick database
  * @abstract
  * @class RubickDB
  */
@@ -17,10 +17,21 @@ abstract class RubickDB {
 	}
 }
 
-/** 用于同步的数据库
- * 为便于数据同步, 数据只会在内存中改动, 持久化需调用 updatePersistence 进行保存
+/** 数据同步层
+ * 数据同步层是一个分布式数据库, 使用基于 yjs 的 CRDT 算法处理数据一致性, 基于 hypercore-protocol 进行P2P通信同步[TODO], 可以看作是一个高效率(https://github.com/dmonad/crdt-benchmarks)的准区块链(节点可信的情况下)
+ *
+ * 特点:
+ * > 可追溯历史数据改动
+ * > 可以离线工作
+ * > 可无限拓展
+ * > 至少有一个节点保存数据, 数据就不会丢失
+ *
+ * 缺点:
+ * > 如果所有数据副本都被销毁, 那么数据就会丢失 ( 可通过固定节点进行备份解决 )
+ * > 数据历史的增加会导致每份数据越来越庞大 ( 可通过中心服务器定期的垃圾回收解决 )
+ *
  */
-export class CRDTDB extends RubickDB {
+export class SyncDB extends RubickDB {
 	private doc!: Y.Doc
 	private persistence: LeveldbPersistence
 	constructor(opt: { path: string; name: string }) {
@@ -33,7 +44,7 @@ export class CRDTDB extends RubickDB {
 		this.doc = await this.persistence.getYDoc(name)
 	}
 
-	// 数据持久化
+	// 数据在同步层内只会在内存中改动, 持久化需调用 updatePersistence 进行保存
 	updatePersistence() {
 		return this.persistence.storeUpdate(this.name, Y.encodeStateAsUpdateV2(this.doc))
 	}
@@ -49,4 +60,34 @@ export class CRDTDB extends RubickDB {
 	}
 }
 
-// todo 本地数据库
+/** [todo] 基于 rxdb 的本地数据库
+ *
+ * 特点:
+ * > 可指定云端进行数据同步
+ * > 数据可加密
+ *
+ */
+export class LocalDB extends RubickDB {
+	db: import('level-rocksdb').LevelUp
+	constructor(opt: { path: string; name: string }) {
+		super(opt)
+		const { path, name } = opt
+		this.db = level(path)
+	}
+
+	get get() {
+		return this.db.get
+	}
+
+	get del() {
+		return this.db.del
+	}
+
+	get set() {
+		return this.db.put
+	}
+
+	get batch() {
+		return this.db.batch
+	}
+}
