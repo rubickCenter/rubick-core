16e056160ee35a83efde872d569fc9f7780bae24
Starry North <gonorth@qq.com>
2021-11-02 10:31:28 +0800

    Update index.ts

--- packages/rubick-adapter-appsearch/src/index.ts
+++ packages/rubick-adapter-appsearch/src/index.ts
@@ -43,12 +43,12 @@ export default class AppSearchAdapter {
     if (process.platform === 'darwin') {
       this.appList = await darwinSearch(this.opt.nativeImage)
     } else if (process.platform === 'linux') {
-      // linux 下直接返回结果
+      // linux 的结果
       this.appList = linuxSearch(
         JSON.parse((await getInstalledApps(true, this.opt.extraDirs)) as string)
       )
     } else if (process.platform === 'win32') {
-      // win 下返回快捷方式列表 所以要再解析
+      // win 的快捷方式列表
       const lnkList = JSON.parse(
         (await getInstalledApps(false, this.opt.extraDirs)) as string
       )
